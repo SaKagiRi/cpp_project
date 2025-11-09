9d8fc07f5938d300c88b9bc3de19d90e7fc64e6d
knakto <kasichonooo@gmail.com>
2025-11-09 16:28:39 +0700

    formatting in file .hpp

--- module07/ex02/Array.hpp
+++ module07/ex02/Array.hpp
@@ -9,12 +9,12 @@ private:
   size_t _size;
 
 public:
-	class out_of_bound: public std::exception {
-		public:
-		virtual const char *what(void) const throw(){
-			return "index out of bound";
-		}
-	};
+  class out_of_bound : public std::exception {
+  public:
+    virtual const char *what(void) const throw() {
+      return "index out of bound";
+    }
+  };
   Array(void) {
     arr = NULL;
     _size = 0;
@@ -22,8 +22,8 @@ public:
   Array(unsigned int n) {
     arr = new T[n];
     _size = n;
-	for (size_t i = 0; i < _size; i++)
-			arr[i] = 0;
+    for (size_t i = 0; i < _size; i++)
+      arr[i] = 0;
   }
   Array(const Array &source) {
     if (this != &source) {
@@ -46,12 +46,12 @@ public:
       for (size_t i = 0; i < _size; i++)
         arr[i] = source.arr[i];
     }
-		(void)source;
+    (void)source;
     return (*this);
   }
   T &operator[](int n) const {
     if (n < 0 || n >= static_cast<int>(_size)) {
-      throw (out_of_bound());
+      throw(out_of_bound());
     }
     return (arr[n]);
   }
