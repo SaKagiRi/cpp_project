bd595c0fc9b614228468955cdfc1ef6085d09a3e
knakto <kasichonooo@gmail.com>
2025-12-25 09:27:49 +0700

    clean and ready to submit eval again

--- module09/ex02/main.cpp
+++ module09/ex02/main.cpp
@@ -33,38 +33,8 @@ int main(int c, char **v)
 	std::cout << "Time to process a range of " RED << a.size() << RESET " elements with " YELLOW "[std::list]  " RESET ": " RED << a.getTime() << RESET " us." << std::endl;
 	std::cout << "Time to process a range of " RED << b.size() << RESET " elements with " YELLOW "[std::deque] " RESET ": " RED << b.getTime() << RESET " us." << std::endl;
 	std::cout << "Time to process a range of " RED << c.size() << RESET " elements with " YELLOW "[std::vector]" RESET ": " RED << c.getTime() << RESET " us." << std::endl;
-
-	// std::cout << YELLOW "------List-----" RESET << std::endl;
-	// // a.printALlItem();
-	// std::cout << a.size() << std::endl;
-	// a.sort();
-	// std::cout << (a.is_sort() ? "sorted." : "not sorted.") << std::endl;
-	// std::cout << "time: " << a.getTime() << std::endl;
-	// // a.printALlItem();
-	// std::cout << YELLOW "===============" RESET << std::endl << std::endl;
-	// std::cout << YELLOW "-----deque-----" RESET << std::endl;
-	// // b.printALlItem();
-	// b.sort();
-	// std::cout << (b.is_sort() ? "sorted." : "not sorted.") << std::endl;
-	// std::cout << "time: " << b.getTime() << std::endl;
-	// // b.printALlItem();
-	// std::cout << YELLOW "===============" RESET << std::endl << std::endl;
-	// std::cout << YELLOW "-----vector----" RESET << std::endl;
-	// // c.printALlItem();
-	// c.sort();
-	// std::cout << (c.is_sort() ? "sorted." : "not sorted.") << std::endl;
-	// std::cout << "time: " << c.getTime() << std::endl;
-	// // c.printALlItem();
-	// std::cout << YELLOW "===============" RESET << std::endl << std::endl;
 	}catch(const std::exception& e)
 	{
 		std::cerr << "Error: " << e.what() << std::endl;
 	}
-	// a.getT().push_back(10);
-	// std::cout <<  a.getT()[0] << std::endl;
-	// PmergeMe<std::vector> b = a;
-	//
-	// b.getT().push_back(11);
-	// // std::cout <<  a.getT()[0] << std::endl;
-	// b.printALlItem();
 }
